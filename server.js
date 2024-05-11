7b818aab10335185de6a0c21aa2567fee30955a7
JeanSandov <128641501+JeanSandov@users.noreply.github.com>
2024-05-11 02:23:33 -0500

    Se crea el servidor mediante el puerto 3010

---
+++ server.js
@@ -0,0 +1,36 @@
+// Importamos express y la conexion a la db.
+const express = require("express");
+const connectDb = require("./db/db");
+// Creamos una instancia de Express.
+
+//Importamos las rutas
+
+const userRoutes = require("./routes/userRoutes");
+const authRoutes = require("./routes/authRoutes");
+const sessionRoutes = require("./routes/sessionRoutes");
+
+const app = express();
+const PORT= 3010;
+
+
+// Middleware
+app.use(express.json()) // Invocamos al middleware para que parsee los datos del body de las solicitudes en formato JSON.
+
+//Rutas de autenticacion
+
+app.use("/api/auth", authRoutes) 
+
+// Rutas de usuarios
+
+app.use("/api/users", userRoutes) // Creamos las rutas de usuario en la ruta /api/users
+
+// Rutas de usuario actual
+
+app.use("/api/session", sessionRoutes) 
+
+// Iniciamos la db.
+connectDb()
+// Inicializamos el servidor y lo ponemos en escucha en el puerto que pusimos arriba.
+app.listen(PORT,()=>{
+    console.log("Servidor corriendo en el puerto: "+ PORT)
+})
